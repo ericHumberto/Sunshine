e9b4b3d41d42651d43101ca39f75db9e416d43c0
unknown <erics@CIT011324CPS.cit>
2016-07-13 17:02:09 -0300

    Mocked populated list adapter

--- app/src/main/java/com/example/erics/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/erics/sunshine/app/MainActivity.java
@@ -6,6 +6,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,6 +38,26 @@ public class MainActivity extends AppCompatActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            // Create some dummy data for the ListView.  Here's a sample weekly forecast
+            String[] data = {
+                    "Mon 6/23 - Sunny - 31/17",
+                    "Tue 6/24 - Foggy - 21/8",
+                    "Wed 6/25 - Cloudy - 22/17",
+                    "Thurs 6/26 - Rainy - 18/11",
+                    "Fri 6/27 - Foggy - 21/10",
+                    "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+                    "Sun 6/29 - Sunny - 20/7"
+            };
+            List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
+
+            ListView listView = (ListView)rootView.findViewById(R.id.list_item_forecast);
+
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(rootView.getContext(),
+                    android.R.layout.simple_list_item_1, weekForecast);
+
+            listView.setAdapter(adapter);
+
             return rootView;
         }
     }

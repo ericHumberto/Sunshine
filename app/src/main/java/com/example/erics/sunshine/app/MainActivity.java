d1db4d628aa7620d2905d9127e7e3003ce86f923
unknown <erics@CIT011324CPS.cit>
2016-07-14 14:08:46 -0300

    Consuming Api Rest Weather
    Signed-off-by: unknown <erics@CIT011324CPS.cit>

--- app/src/main/java/com/example/erics/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/erics/sunshine/app/MainActivity.java
@@ -1,17 +1,7 @@
 package com.example.erics.sunshine.app;
 
-import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,44 +11,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
-        }
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-            // Create some dummy data for the ListView.  Here's a sample weekly forecast
-            String[] data = {
-                    "Mon 6/23 - Sunny - 31/17",
-                    "Tue 6/24 - Foggy - 21/8",
-                    "Wed 6/25 - Cloudy - 22/17",
-                    "Thurs 6/26 - Rainy - 18/11",
-                    "Fri 6/27 - Foggy - 21/10",
-                    "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
-                    "Sun 6/29 - Sunny - 20/7"
-            };
-            List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
-
-            ListView listView = (ListView)rootView.findViewById(R.id.list_item_forecast);
-
-            ArrayAdapter<String> adapter = new ArrayAdapter<String>(rootView.getContext(),
-                    android.R.layout.simple_list_item_1, weekForecast);
-
-            listView.setAdapter(adapter);
-
-            return rootView;
         }
     }
 }
